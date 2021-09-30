d4476fb7b9dad125a25e67ab3c0c85d1608818fe
sciyen <a885566885566@gmail.com>
2021-10-01 00:28:58 +0800

    Add navbar click event and enhance responsive behavior
    1. And navbar event to guide user to correct page.
    2. Add the reponsive behaviors for navbar and content width.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,13 @@ var md = window.markdownit();
     }
 
     function set_tab_display(intro_tag, content_tag, id) {
+        /* Set the click event of switching pages
+
+        Args:
+            intro_tag: the id of button to be trigger.
+            content_tag: the id of content to be show.
+            id: the identifying number of the content.
+         */
         var count = $(content_tag).children('div').length
         for (i = 0; i < count; i++) {
             $(content_tag + ' #tab-' + i).css('display', (i == id) ? 'block' : 'none')
@@ -56,19 +63,29 @@ var md = window.markdownit();
         $(selector).append(md.render(content))
     }
 
-    function navigator_generator(nav_tag, content) {
+    function navigator_generator(nav_tag, content, intro_tag, content_tag) {
+        /* Generate the navbar items and bind the clicking event
+
+        Args: 
+            nav_tag: the id of the navbar container. The navbar content will be append in it.
+            content: the article to be analyzed.
+            intro_tag: the tag of the container of the tab header.
+            content_tag: the tag of the container of the content.
+         */
         btn_title = content.match(md_rule['identifier1'])[0].slice(2, -1);
         var sub_btn = $('<ul></ul>')
 
         sub_content = content.split(md_rule['section'])
-        sub_content.forEach((sub) => {
+        sub_content.forEach((sub, idx) => {
             console.log(sub)
             sub_title = sub.match(md_rule['title_filter2'])
             if (sub_title != null)
                 sub_btn.append(
                     $('<li></li>').append(
                         $('<a></a>')
-                            .attr('href', '#sub-btn-' + sub_title[0].slice(2, -1))
+                            //.attr('href', '#sub-' + get_page_id(sub))
+                            .on('click', function () { set_tab_display(intro_tag, content_tag, idx - 1) })
+                            .attr('href', '#btn-' + btn_title)
                             .text(sub_title[0].slice(2, -1))
                     )
                 )
@@ -86,12 +103,18 @@ var md = window.markdownit();
         )
     }
 
+    function get_page_id(article) {
+        return 'btn-' + article.match(md_rule['identifier1'])[0].slice(2, -1)
+    }
+
     var tab_handler = {
         '團隊介紹': (contents) => {
+            $('#page-team').attr('id', get_page_id(contents))
             navigator_generator('#navigator-container', contents)
             content_render(contents, '#content-team')
         },
         '各組介紹': (contents) => {
+            //$('#content-team').attr('id', contents.match(md_rule['identifier2']))
             sections = contents.split(md_rule['section'])
             // headers
 
@@ -101,18 +124,22 @@ var md = window.markdownit();
 
         },
         '活動': function (contents) {
-            navigator_generator('#navigator-container', contents)
+            $('#page-activities').attr('id', get_page_id(contents))
+            navigator_generator('#navigator-container', contents, '#activities-intro', '#activities-articles')
             article_parser(contents, '#activities-intro', '#activities-articles')
         },
         '專案計畫': function (contents) {
-            navigator_generator('#navigator-container', contents)
+            $('#page-projects').attr('id', get_page_id(contents))
+            navigator_generator('#navigator-container', contents, '#projects-intro', '#projects-articles')
             article_parser(contents, '#projects-intro', '#projects-articles')
         },
         '歷屆作品/商品': (contents) => {
-            navigator_generator('#navigator-container', contents)
+            $('#page-products').attr('id', get_page_id(contents))
+            navigator_generator('#navigator-container', contents, '#products-intro', '#products-articles')
             article_parser(contents, '#products-intro', '#products-articles')
         },
         '聯絡我們': (contents) => {
+            $('#page-contact').attr('id', get_page_id(contents))
             navigator_generator('#navigator-container', contents)
             content_render(contents, '#content-contact')
         }
