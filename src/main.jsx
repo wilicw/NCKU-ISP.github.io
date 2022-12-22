edeac2ad0c0aa4680b71595207e54ebeeaf45ced
William Chang <wilicw1346@gmail.com>
2022-12-22 19:55:54 +0800

    feat: add phone menu button

--- src/main.jsx
+++ src/main.jsx
@@ -23,17 +23,24 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="mx-auto pt-2">
       <div className='mx-auto max-w-5xl flex items-center'>
-        <img src="/logo.svg" className="h-20 p-5 inline right" />
+        <img src="/logo.svg" className="h-20 p-4 inline right" />
         <div className="text-2xl font-medium">NCKU ISP</div>
-        <div className='mr-0 ml-auto'>
+        <div className='mr-0 ml-auto hidden lg:block xl:block 2xl:block'>
           <NavItem text="About Us" />
           <NavItem text="Teams" />
           <NavItem text="Mission" />
           <NavItem text="Projects" />
           <button className="font-semibold mx-3 p-2 rounded-md inline bg-blue-500 hover:bg-blue-600">Support Us</button>
         </div>
+        <div className='mr-0 ml-auto block lg:hidden xl:hidden 2xl:hidden'>
+          <button className="font-semibold mx-3 p-2 rounded-md inline">
+            <i className="gg-menu-right"></i>
+          </button>
+        </div>
       </div>
     </div>
-    <RouterProvider router={router} />
+    <div className='p-5'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
