548df1e351961a542e6480d594c17682ce30fde4
William Chang <wilicw1346@gmail.com>
2023-02-05 23:30:19 +0800

    feat(stye): change menu font

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 
 function App() {
   const [menu, setMenu] = useState(false);
-  const NavItem = ({ text, url }) => <Link to={url} className="font-medium mx-3 inline">{text}</Link>;
+  const NavItem = ({ text, url }) => <Link to={url} className="font-bold mx-3 inline">{text}</Link>;
   const NavItemData = [
     {
       text: 'About Us',
@@ -27,7 +27,7 @@ function App() {
     <>
       <div className="mx-auto pt-2">
         <div className='mx-auto max-w-5xl flex items-center font-semibold uppercase'>
-          <Link to="/" className="text-2xl decorative">
+          <Link to="/" onClick={()=>setMenu(false)} className="text-2xl decorative">
             <img src="/logo.svg" className="h-20 p-4 inline right" />
             NCKU ISP
           </Link>
@@ -43,7 +43,7 @@ function App() {
             </a>
           </div>
           <div className='mr-0 ml-auto block lg:hidden xl:hidden 2xl:hidden'>
-            <button className="font-semibold mx-3 p-2 rounded-md inline" onClick={() => setMenu(m => !m)}>
+            <button className="mx-3 p-2 rounded-md inline" onClick={() => setMenu(m => !m)}>
               {
                 menu ? <i className="gg-close"></i> : <i className="gg-menu-right"></i>
               }
@@ -54,12 +54,12 @@ function App() {
           menu ? (
             <div className="container text-center" >
               {NavItemData.map(item => (
-                <Link className="block mx-auto py-2" to={item.url}>
+                <Link className="block mx-auto py-2 font-medium" onClick={()=>setMenu(false)} to={item.url}>
                   {item.text}
                 </Link>
               ))}
               <a
-                className="p-2 rounded-md block font-semibold"
+                className="p-2 rounded-md block font-black"
                 href="https://donate2.ncku.edu.tw/index.php?c=donate&m=step1&usename=%E5%A4%AA%E7%A9%BA%E6%8E%A8%E9%80%B2%E7%A0%94%E7%A9%B6%E7%A4%BE&usetype=ut9"
                 target="_blank"
                 rel="noopener noreferrer"
