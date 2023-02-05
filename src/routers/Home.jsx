89e07f88ddb180a2c04dca6402475815c0cf4224
William Chang <wilicw1346@gmail.com>
2023-02-05 22:47:14 +0800

    feat(home): change slogan

--- src/routers/Home.jsx
+++ src/routers/Home.jsx
@@ -7,9 +7,15 @@ function Home() {
         <video autoPlay muted loop>
           <source src="background.mp4" type="video/mp4" />
         </video>
-        <div className='mx-auto max-w-5xl flex items-center' style={{ height: "75vh" }}>
+        <div className='mx-auto max-w-5xl flex items-center' style={{ height: "70vh" }}>
           <div>
-            <p className="text-8xl font-black py-5">一飛，<br className='block md:hidden sm:hidden' />沖天。</p>
+            <p className="text-7xl font-black py-5">
+              <i className="gg-quote inline-block h-12" style={{ transform: "scale(2)" }}></i>
+              GO ABOVE
+              <br />
+              AND BEYOND
+              <i className="gg-quote inline-block" style={{ transform: "scale(2)" }}></i>
+            </p>
             <p className="text-2xl font-bold">由成大學生組成的火箭開發團隊</p>
           </div>
         </div>
