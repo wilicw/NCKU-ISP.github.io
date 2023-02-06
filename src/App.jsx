50597fd2b8c301d47d146cf4f13d8e9260e716b6
William Chang <wilicw1346@gmail.com>
2023-02-06 14:22:39 +0800

    feat(home): move footer to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     <>
       <div className="mx-auto pt-2">
         <div className='mx-auto max-w-5xl flex items-center font-semibold uppercase'>
-          <Link to="/" onClick={()=>setMenu(false)} className="text-2xl decorative">
+          <Link to="/" onClick={() => setMenu(false)} className="text-2xl decorative">
             <img src="/logo.svg" className="h-20 p-4 inline right" />
             NCKU ISP
           </Link>
@@ -54,7 +54,7 @@ function App() {
           menu ? (
             <div className="container text-center" >
               {NavItemData.map(item => (
-                <Link className="block mx-auto py-2 font-medium" onClick={()=>setMenu(false)} to={item.url}>
+                <Link className="block mx-auto py-2 font-medium" onClick={() => setMenu(false)} to={item.url}>
                   {item.text}
                 </Link>
               ))}
@@ -72,7 +72,25 @@ function App() {
       </div>
       <div className='p-5'>
         <Outlet />
-        <p className="text-center pt-10 pb-5">© { new Date().getFullYear() } NCKU ISP</p>
+        <i class="gg-chevron-double-down mx-auto my-10"></i>
+        <div className='bg-zinc-100 mx-auto max-w-5xl rounded-xl px-5 py-10 text-center' style={{ color: "#383838" }}>
+          <p className='text-xl font-bold text-center pb-5'>聯絡我們</p>
+          <p className='py-1'>國立成功大學 太空推進研究社</p>
+          <p className='py-1 font-medium'>Institute of Space Propulsion, NCKU</p>
+          <div className='flex items-center mx-auto justify-center gap-10 py-5'>
+            <a href="https://www.facebook.com/ispncku" target="_blank"><i className="gg-facebook"></i></a>
+            <a href="https://www.instagram.com/ispncku/" target="_blank"><i className="gg-instagram"></i></a>
+            <a href="https://github.com/NCKU-ISP" target="_blank"><i className="gg-code-slash"></i></a>
+            <a href="mailto:aerogroup2020@gmail.com" target="_blank"><i className="gg-mail"></i></a>
+          </div>
+          <p className='py-1'>701401 臺南市東區大學路一號</p>
+          <p className='py-1'>國立成功大學 光復校區 學生活動中心4樓</p>
+          <div className='mx-auto flex gap-5 justify-center items-center text-xs text-gray-600 pt-8'>
+            <a href="https://archive.nckuisp.space/" target="_blank">舊版網頁</a>
+            <a href="https://opensource.org/licenses/BSD-3-Clause" target="_blank">Open Source License</a>
+          </div>
+        </div>
+        <p className="text-center pt-10 pb-5">&copy; {new Date().getFullYear()} NCKU ISP</p>
       </div>
     </>
   );
--- src/routers/Home.jsx
+++ src/routers/Home.jsx
@@ -30,19 +30,6 @@ function Home() {
           <br />
         </div>
       </div>
-      <div className='bg-zinc-100 shadow-lg mx-auto max-w-5xl rounded-xl p-10 text-center' style={{ color: "#383838" }}>
-        <p className='text-xl font-bold text-center pb-5'>聯絡我們</p>
-        <p className='py-1'>國立成功大學 太空推進研究社</p>
-        <p className='py-1 font-medium'>Institute of Space Propulsion, NCKU</p>
-        <div className='flex items-center mx-auto justify-center gap-10 pt-5 pb-10'>
-          <a href="https://www.facebook.com/ispncku" target="_blank"><i className="gg-facebook"></i></a>
-          <a href="https://www.instagram.com/ispncku/" target="_blank"><i className="gg-instagram"></i></a>
-          <a href="https://github.com/NCKU-ISP" target="_blank"><i className="gg-code-slash"></i></a>
-          <a href="mailto:aerogroup2020@gmail.com" target="_blank"><i className="gg-mail"></i></a>
-        </div>
-        <p className='py-1'>701401 臺南市東區大學路一號</p>
-        <p className='py-1'>國立成功大學 光復校區 學生活動中心4樓</p>
-      </div>
     </>
   );
 }
