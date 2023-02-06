4c4dc7369573c9014bf0b3b3ba1ea81761ed852a
William Chang <wilicw1346@gmail.com>
2023-02-06 15:09:05 +0800

    feat(style): change padding size

--- src/routers/About.jsx
+++ src/routers/About.jsx
@@ -1,14 +1,14 @@
 function About() {
   return (
     <>
-      <div className='bg-zinc-100 mx-auto max-w-5xl rounded-xl p-10 my-10' style={{ color: "#383838" }}>
+      <div className='bg-zinc-100 mx-auto max-w-5xl rounded-xl p-8 mb-10' style={{ color: "#383838" }}>
         <p className='text-3xl font-bold pb-3'>團隊介紹</p>
         <hr className="border-sky-400 rounded-sm w-10 border-b-2" />
         <p className="py-5">我們是來自成功大學的 ISP 太空推進研究社 (Institute of Space Propulsion)。ISP 是一個由成大不同系所的學生自發性成立的社團，並且致力於研究火箭技術。在成立的這三年間，ISP 無論在研究、飛行測試、課程及推廣皆有著墨。這個團隊充滿著富有熱誠、對未來有著無限憧憬且不乏實力的青年們。</p>
         <img src="https://i.imgur.com/S0hVyit.jpg" className="block rounded-xl" alt="" />
       </div>
 
-      <div className='bg-zinc-100 mx-auto max-w-5xl rounded-xl p-10 my-10' style={{ color: "#383838" }}>
+      <div className='bg-zinc-100 mx-auto max-w-5xl rounded-xl p-8 my-10' style={{ color: "#383838" }}>
         <p className='text-3xl font-bold pb-3'>加入我們</p>
         <hr className="border-sky-400 rounded-sm w-10 border-b-2" />
         <p className="text-gray-600 font-bold py-5">
