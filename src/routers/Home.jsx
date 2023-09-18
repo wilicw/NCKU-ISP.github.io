fbf16dc9765d891e43d9263aa287369751180676
William Chang <wilicw1346@gmail.com>
2023-09-18 18:16:13 +0800

    fix: complete sponsorship

--- src/routers/Home.jsx
+++ src/routers/Home.jsx
@@ -9,6 +9,8 @@ import logo_HackMD from "../logos/HackMD.svg";
 import logo_nckues from "../logos/nckues.png";
 import logo_epolab from "../logos/epolab.jpg";
 import logo_tanko from "../logos/tanko.jpg";
+import logo_hw from "../logos/hw.svg";
+import logo_aeas from "../logos/aeas.png";
 
 function Logo({ url, label, logo_image }) {
   return <a
@@ -69,10 +71,31 @@ function Home() {
           <Logo url="https://hackmd.io/" logo_image={logo_HackMD} label="HackMD" />
           <Logo url="https://www.cmsteel.com.tw/" logo_image={logo_cmsteel} label="澄茂企業股份有限公司" />
           <Logo url="http://fund.es.ncku.edu.tw/esfd/" logo_image={logo_nckues} label="國立成功大學工程科學文教基金會" />
+          <Logo url="https://www.aeas.space/" logo_image={logo_aeas} label="AEAS 逆熵航太學會" />
           <Logo url="https://www.epolab.com/" logo_image={logo_epolab} label="大格化學工業股份有限公司" />
           <Logo url="https://www.tanko.com.tw/" logo_image={logo_tanko} label="天鋼事業股份有限公司" />
+          <Logo url="https://www.ohmplus.com/" logo_image={logo_ohmplus} label="歐姆佳科技股份有限公司" />
+          <Logo url="mailto:alma.hwvision@gmail.com" logo_image={logo_hw} label="柏拓有限公司" />
         </div>
         <br />
+        <p className="text-2xl pb-3 font-bold">個人贊助</p>
+        <hr className="border-sky-400 w-10 border-b-2 rounded-sm" />
+        <div className="flex items-center pt-5 gap-5 flex-wrap justify-center">
+          <p>蔡淑櫻</p>
+          <p>楊智量</p>
+          <p>江俊慧</p>
+          <p>吳承翰</p>
+          <p>黃仁志</p>
+          <p>蘇尹廷</p>
+        </div>
+        <div className="flex items-center pt-5 gap-5 flex-wrap justify-center">
+          <p>徐麗珍</p>
+          <p>朱盈禔</p>
+          <p>鄭舜之</p>
+          <p>饒振奇</p>
+          <p>吳敏求</p>
+          <p>黃偉祥</p>
+        </div>
         <p className="text-xs font-bold text-gray-400 text-right">*此清單依據合作時間排序</p>
       </div>
     </>
